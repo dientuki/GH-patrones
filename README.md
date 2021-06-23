223c2a4a0ad01e0d93c4891b06a72a847f25e009
Juan "Dientuki" Farias <dientuki@gmail.com>
2021-06-23 12:25:47 -0300

    readme

--- README.md
+++ README.md
@@ -3,11 +3,6 @@ Instalar
 
 npm install
 
-Renombrar los archivos:
-
-- public/classes.example.js => public/classes.js
-- public/main.example.js    => public/main.js
-
 Correr
 ======
 
@@ -18,3 +13,20 @@ npm run dev
 Si no lo tenemos instalado:
 
 Abrímos el archivo public/index.html con un browser.
+
+Revisar
+======
+
+Hay 2 equipos de naves
+
+- Tienen nave grande y chica
+- Nave grande se mueve vertical
+- Nave chica se mueve horizontal
+- Cada equipo tiene una nave chica que se mueve random
+- Las naves chicas tienen armas sniper
+- Las naves grandes tienen armas random
+- Una nave chica por equipo se mueve esquivando
+- Hay un capitan que si muere, muere todo el equipo
+- Hay una parca que avisa quien murio
+
+Hay un tercer equipo de naves angostas que son "basura espacial" que hace daño a cualquiera, cuando la basura muere otra vuelve a nacer
--- public/main.js
+++ public/main.js
@@ -11,16 +11,14 @@ function setup() {
   for(let i = 0; i<2; i++) {
     naves1.push(new NaveChica( rand(5, CANVAS.x-5), rand(5, CANVAS.y-5), new MovedorHorizontal(), new ArmaSnipper(naves1, naves2)));
     naves1.push(new NaveGrande( rand(10, CANVAS.x-10), rand(10, CANVAS.y-10),new MovedorVertical(), new ArmaRandom(naves1)));
-    //naves1.push(new NaveAngosta( rand(0, CANVAS.x), rand(0, CANVAS.y-5)));
   }
-  naves1.push(new NaveChica( rand(5, CANVAS.x-5), rand(5, CANVAS.y-5),new MovedorEsquivador(new MovedorVertical(), naves1)));
+  naves1.push(new NaveChica( rand(5, CANVAS.x-5), rand(5, CANVAS.y-5),new MovedorEsquivador(new MovedorRandom(), naves1)));
 
   for(let i = 0; i<2; i++) {
     naves2.push(new NaveChica( rand(5, CANVAS.x-5), rand(5, CANVAS.y-5),new MovedorHorizontal(),new ArmaSnipper(naves2, naves1)));
-    naves2.push(new NaveGrande( rand(10, CANVAS.x-10), rand(10, CANVAS.y-10),new MovedorRandom(), new ArmaRandom(naves1) ));
-    //naves2.push(new NaveAngosta( rand(0, CANVAS.x), rand(0, CANVAS.y-5)));
+    naves2.push(new NaveGrande( rand(10, CANVAS.x-10), rand(10, CANVAS.y-10),new MovedorVertical(), new ArmaRandom(naves1) ));
   }
-  naves2.push(new NaveChica( rand(5, CANVAS.x-5), rand(5, CANVAS.y-5),new MovedorEsquivador(new MovedorVertical(), naves1)));
+  naves2.push(new NaveChica( rand(5, CANVAS.x-5), rand(5, CANVAS.y-5),new MovedorEsquivador(new MovedorRandom(), naves1)));
 
   for(let i = 0; i<8; i++) {
     debris.push(new NaveAngosta( rand(0, CANVAS.x), rand(0, CANVAS.y-5)));
